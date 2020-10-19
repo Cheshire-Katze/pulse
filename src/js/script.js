349dea8b00b14b29274793dc5cff6d7e1131b967
Cheshire-Katze <serj.older@gmail.com>
2020-10-19 17:37:23 +0300

    slider succ

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,17 @@
-alert(1);
-[0,3].push() 
+$(document).ready(function () {
+  $('.carousel__inner').slick({
+    speed: 1200,
+    // adaptiveHeight: true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></button>',
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          dots: true,
+          arrows: false
+        }
+      }
+    ]
+  });
+});
