b5f3b97ea9df444209db6487a5962bde579fd813
Cheshire-Katze <serj.older@gmail.com>
2020-10-20 16:41:35 +0300

    разные карусели

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,34 @@
-$(document).ready(function () {
-  $('.carousel__inner').slick({
-    speed: 1200,
-    // adaptiveHeight: true,
-    prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></button>',
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          dots: true,
-          arrows: false
-        }
-      }
-    ]
-  });
+// $(document).ready(function () {
+//   $('.carousel__inner').slick({
+//     speed: 1200,
+//     // adaptiveHeight: true,
+//     prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
+//     nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></button>',
+//     responsive: [
+//       {
+//         breakpoint: 768,
+//         settings: {
+//           dots: true,
+//           arrows: false
+//         }
+//       }
+//     ]
+//   });
+// }); для slick
+
+const slider = tns({
+  container: '.carousel__inner',
+  items: 1, // кол-во слайдов в карусели одновременно
+  slideBy: 'page',
+  autoplay: false,
+  controls: false, // скрыть родные кнопки tiny-slider
+  nav: false
 });
+
+document.querySelector('.prev').onclick = function () {
+  slider.goTo('prev');
+};
+
+document.querySelector('.next').onclick = function () {
+  slider.goTo('next');
+};
