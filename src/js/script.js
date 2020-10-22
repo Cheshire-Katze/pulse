7c229b2c2190461eaa8e5a9b16817e95a73e44c2
Cheshire-Katze <serj.older@gmail.com>
2020-10-22 16:18:05 +0300

    блок с табами готов/также моб. версия

--- src/js/script.js
+++ src/js/script.js
@@ -1,36 +1,55 @@
 // для slick
-// $(document).ready(function () {
-//   $('.carousel__inner').slick({
-//     speed: 1200,
-//     // adaptiveHeight: true,
-//     prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
-//     nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></button>',
-//     responsive: [
-//       {
-//         breakpoint: 768,
-//         settings: {
-//           dots: true,
-//           arrows: false
-//         }
-//       }
-//     ]
-//   });
-// }); 
+$(document).ready(function () {
+  $('.carousel__inner').slick({
+    speed: 1200,
+    // adaptiveHeight: true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></button>',
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          dots: false,
+          arrows: false
+        }
+      }
+    ]
+  });
 
-// для tiny-slider
-const slider = tns({
-  container: '.carousel__inner',
-  items: 1, // кол-во слайдов в карусели одновременно
-  slideBy: 'page',
-  autoplay: false,
-  controls: false, // скрыть родные кнопки tiny-slider
-  nav: false
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+  });
+
+  function toggleSlide(item) {
+    $(item).each(function (i) {
+      $(this).on('click', function (e) {
+        e.preventDefault();
+        $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+        $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+      })
+    });
+  };
+
+  toggleSlide('.catalog-item__link');
+  toggleSlide('.catalog-item__back');
 });
 
-document.querySelector('.prev').onclick = function () {
-  slider.goTo('prev');
-};
+// для tiny-slider
+// const slider = tns({
+//   container: '.carousel__inner',
+//   items: 1, // кол-во слайдов в карусели одновременно
+//   slideBy: 'page',
+//   autoplay: false,
+//   controls: false, // скрыть родные кнопки tiny-slider
+//   nav: false
+// });
 
-document.querySelector('.next').onclick = function () {
-  slider.goTo('next');
-};
+// document.querySelector('.prev').onclick = function () {
+//   slider.goTo('prev');
+// };
+
+// document.querySelector('.next').onclick = function () {
+//   slider.goTo('next');
+// };
